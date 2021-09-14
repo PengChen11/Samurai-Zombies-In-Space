d2a34cea62cc15d0a8c87d7185766983d209d1f5
PengChen11 <peng.chen11@outlook.com>
2021-09-13 20:26:27 -0700

    get command finished

--- src/main/java/com/gameEngine/commands/GETCommand.java
+++ src/main/java/com/gameEngine/commands/GETCommand.java
@@ -1,5 +1,9 @@
 package com.gameEngine.commands;
 
+import com.character.Player;
+import com.item.Item;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +11,25 @@ public class GETCommand implements CommandInterface{
 
     @Override
     public void processCommand(StringBuilder gameBuilder, String[] command, Map<String, Map<String, List<String>>> instructs) {
+        processGet(gameBuilder, command, instructs);
+    }
 
+    private void processGet(StringBuilder gameBuilder, String[] command, Map<String, Map<String, List<String>>> instructs) {
+        if (command.length > 1) {
+            gameBuilder.append(pickUpItem(command[1], instructs));
+        } else {
+            gameBuilder.append(instructs.get("get").get("instructions").get(0));
+        }
+    }
+
+    private String pickUpItem(String thing,Map<String, Map<String, List<String>>> instructs ) {
+        Item item = Player.PLAYER.getCurrentLocation().containsItem(thing);
+        if (item != null) {
+            Player.PLAYER.addToInventory(item);
+            Player.PLAYER.getCurrentLocation().removeItem(item);
+            return instructs.get("pick").get("instructions").get(0) + thing + instructs.get("pick").get("instructions").get(1);
+        }
+
+        return thing + instructs.get("pick").get("instructions").get(3);
     }
 }
