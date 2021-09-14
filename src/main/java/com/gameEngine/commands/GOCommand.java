36d3c16ebe9f982cd092711125df9a54cae4bd6e
PengChen11 <peng.chen11@outlook.com>
2021-09-13 18:18:53 -0700

    look command updated

--- src/main/java/com/gameEngine/commands/GOCommand.java
+++ src/main/java/com/gameEngine/commands/GOCommand.java
@@ -2,8 +2,6 @@ package com.gameEngine.commands;
 
 import com.character.Player;
 import com.location.Locations;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import java.util.List;
 import java.util.Map;
@@ -11,19 +9,9 @@ import java.util.Map;
 public class GOCommand implements CommandInterface{
     @Override
     public void processCommand(StringBuilder gameBuilder, String[] command, Map<String, Map<String, List<String>>> instructs ) {
-        processGo(gameBuilder, command[1],instructs);
-
-    }
-
-    private void processGo(StringBuilder gameBuilder, String direction, Map<String, Map<String, List<String>>> instructs) {
-        gameBuilder.append(headToNextRoom(direction, instructs));
-    }
-
-
-    private String headToNextRoom(String direction,Map<String, Map<String, List<String>>> instructs) {
 
         Locations nextLocation = null;
-        switch (direction.toLowerCase()){
+        switch (command[1].toLowerCase()){
             case "north":
                 nextLocation = Player.PLAYER.getCurrentLocation().getNorth();
                 break;
@@ -39,10 +27,12 @@ public class GOCommand implements CommandInterface{
         }
 
         if (nextLocation == null){
-            return instructs.get("head").get("instructions").get(2);
+            gameBuilder.append(instructs.get("head").get("instructions").get(2));
+        } else {
+            gameBuilder.append(instructs.get("head").get("instructions").get(0)).append(command[1]).append("\n");
         }
         Player.PLAYER.setCurrentLocation(nextLocation);
-        return instructs.get("head").get("instructions").get(0) + direction + "\n";
+
     }
 
 }
--- src/main/java/com/gameEngine/commands/LOOKCommand.java
+++ src/main/java/com/gameEngine/commands/LOOKCommand.java
@@ -1,5 +1,10 @@
 package com.gameEngine.commands;
 
+import com.character.NPC;
+import com.character.Player;
+import com.character.Zombie;
+import com.item.Item;
+import com.sound.Roar;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +12,63 @@ public class LOOKCommand implements CommandInterface {
 
     @Override
     public void processCommand(StringBuilder gameBuilder, String[] command, Map<String, Map<String, List<String>>> instructs) {
+        processLook(gameBuilder, command, instructs);
+    }
 
+    private void processLook(StringBuilder gameBuilder, String[] command, Map<String, Map<String, List<String>>> instructs) {
+        if (command[1] == null || command[1].isBlank() || "around".equalsIgnoreCase(command[1])) {
+            gameBuilder.append(Player.PLAYER.getCurrentLocation().getDescription()).append("\n");
+            appendItemsToDescription(gameBuilder, instructs);
+        } else {
+            gameBuilder.append(getLookResult(command[1],instructs));
+        }
+        // the zombie should attack you if there is a zombie in the room
+        processZombieAttack(gameBuilder);
+    }
+
+
+    private void appendItemsToDescription(StringBuilder description, Map<String, Map<String, List<String>>> instructs) {
+        List<Item> items = Player.PLAYER.getCurrentLocation().getItemList();
+
+        if (items.size() > 0) {
+            description.append(instructs.get("examine").get("instructions").get(0));
+            for (Item item : items) {
+                description.append(" ").append(item.getName()).append(", ");
+            }
+            description.append("]\n");
+        } else {
+            description.append(instructs.get("examine").get("instructions").get(1));
+        }
+    }
+
+    private void processZombieAttack(StringBuilder gameBuilder) {
+        Zombie zombie = Player.PLAYER.getCurrentLocation().getZombie();
+        if ( zombie != null && zombie.getHealth() > 0) {
+            new Roar().startMusic();
+            Player.PLAYER.takeDamage(zombie.attack());
+            gameBuilder.append("\nThe Zomburai hits you. You have ").append(Player.PLAYER.getHealth()).append("HP.");
+            gameBuilder.append("\nYou can fight the zombie or go to other locations");
+        }
+    }
+
+    private String getLookResult(String objectToFind, Map<String, Map<String, List<String>>> instructs) {
+        String response;
+        Item item = containsItem(objectToFind);
+        if (item != null) {
+            response = item.getDescription();
+        } else {
+            response = instructs.get("look").get("instructions").get(1) + objectToFind + ".";
+        }
+        return response + "\n";
+    }
+
+    private Item containsItem(String key){
+        List<Item> items = Player.PLAYER.getCurrentLocation().getItemList();
+        for (Item item : items){
+            if (key.equalsIgnoreCase(item.getName())){
+                return item;
+            }
+        }
+        return null;
     }
 }
