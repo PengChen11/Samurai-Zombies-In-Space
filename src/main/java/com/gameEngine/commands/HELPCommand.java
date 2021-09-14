f7381a9926d7daffd80a033b46157e4a51d07f04
PengChen11 <peng.chen11@outlook.com>
2021-09-13 20:48:43 -0700

    help command done

--- src/main/java/com/gameEngine/commands/HELPCommand.java
+++ src/main/java/com/gameEngine/commands/HELPCommand.java
@@ -8,6 +8,13 @@ public class HELPCommand implements CommandInterface{
 
     @Override
     public void processCommand(StringBuilder gameBuilder, String[] command, Map<String, Map<String, List<String>>> instructs) {
+        gameBuilder.append(showInstructions(instructs));
+    }
 
+    private StringBuilder showInstructions( Map<String, Map<String, List<String>>> instructs) {
+        StringBuilder builder = new StringBuilder();
+        //for each line, append it to the string builder object
+        instructs.get("help").get("instructions").forEach(eachLine-> builder.append("\n").append(eachLine));
+        return builder;
     }
 }
